6c4f7ec4f558c3fd8cb806a927c04fe4852d980d
tuagkcuf <jokersms222@gmail.com>
2023-02-11 14:12:24 +0300

    Some css changes, adding images, installing style

--- src/App.js
+++ src/App.js
@@ -1,1 +1,56 @@
-import Spline from "@splinetool/react-spline"
+import Spline from '@splinetool/react-spline';
+import styled from "styled-components"
+
+export default function App() {
+  return (
+    <Wrapper>
+        <Spline className='spline' scene="https://prod.spline.design/rxX2nQBZldBimjFX/scene.splinecode" />
+        <Content>
+            <h1>Collaborate with people</h1>   
+            <p>Bring your team together and build your community by using our cross-platform</p>
+        </Content>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+    font-family: 'Spline Sans', sans-serif;
+    font-size: 16px;
+    color: white;
+    position: relative;
+
+    .spline {
+        position: absolute;
+        margin: 0;
+        top: 0;
+        right: 0;
+    }
+`;
+
+const Content = styled.div`
+    position: absolute;
+    top: 30px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+
+    h1 {
+        font-family: 'Spline Sans Mono', monospace;
+        font-weight: bold;
+        font-size: 70px;
+        margin: 0;
+        max-width: 500px;
+    }
+
+    p {
+        font-weight: normal;
+        line-height: 150%;
+        max-width: 380px;
+    }
+
+    h1, p {
+        margin: 0 30px 0 100px;
+    }
+`
+
