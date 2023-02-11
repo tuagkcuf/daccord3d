dec5072ec17c56ba0e371974f1b72ed53144a3d6
tuagkcuf <jokersms222@gmail.com>
2023-02-11 15:13:58 +0300

    User friendly view, device friendly view

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import Spline from '@splinetool/react-spline';
-import styled from "styled-components"
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
+
+import Logo from "./images/logo.svg";
+import IconTwitter from "./images/icon-twitter.svg";
+import IconYoutube from "./images/icon-youtube.svg";
+import IconLaptop from "./images/icon-laptop.svg";
 
 export default function App() {
-  return (
-    <Wrapper>
-        <Spline className='spline' scene="https://prod.spline.design/rxX2nQBZldBimjFX/scene.splinecode" />
-        <Content>
-            <h1>Collaborate with people</h1>   
-            <p>Bring your team together and build your community by using our cross-platform</p>
-        </Content>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Spline
+                className="spline"
+                scene="https://prod.spline.design/rxX2nQBZldBimjFX/scene.splinecode"
+            />
+            <Content>
+                <Menu>
+                    <li>
+                        <img src={Logo} alt="Logo" />
+                    </li>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Donwload</a>
+                    </li>
+                    <li>
+                        <a href="/">App</a>
+                    </li>
+                    <li>
+                        <a href="/">Login</a>
+                    </li>
+                    <li>
+                        <button>Get Started</button>
+                    </li>
+                </Menu>
+                <h1>Collaborate with people</h1>
+                <p>
+                    Bring your team together and build your community by using
+                    our cross-platform. Join the private beta
+                </p>
+                <button>
+                    <img src={IconLaptop} alt="Download" /> Download for Mac
+                </button>
+            </Content>
+            <Social>
+                <div />
+                <img src={IconTwitter} alt="Twitter" />
+                <img src={IconYoutube} alt="YouTube" />
+            </Social>
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled.div`
-    font-family: 'Spline Sans', sans-serif;
+    font-family: "Spline Sans", sans-serif;
     font-size: 16px;
     color: white;
     position: relative;
@@ -24,6 +63,22 @@ const Wrapper = styled.div`
         margin: 0;
         top: 0;
         right: 0;
+
+        @media (max-width: 1024px) {
+            transform: scale(0.8) translateX(200px);
+        }
+        @media (max-width: 800px) {
+            transform: scale(0.7) translateX(600px);    
+        }
+        @media (max-width: 600px) {
+            transform: scale(0.5) translateX(-100px);
+            right: auto;
+            left: 50%;
+            margin-left: -600px;
+        }
+        @media (max-width: 375px) {
+            transform: scale(0.45) translateX(-50px);
+        }
     }
 `;
 
@@ -35,12 +90,28 @@ const Content = styled.div`
     flex-direction: column;
     gap: 80px;
 
+    @media (max-width: 1024px) {
+        gap: 40px;
+    }
+
     h1 {
-        font-family: 'Spline Sans Mono', monospace;
+        font-family: "Spline Sans Mono", monospace;
         font-weight: bold;
         font-size: 70px;
         margin: 0;
         max-width: 500px;
+
+        @media (max-width: 1024px) {
+            font-size: 60px;
+            max-width: 400px;
+        }
+        @media (max-width: 800px) {
+            font-size: 40px;
+            max-width: 300px;
+        }
+        @media (max-width: 600px) {
+            padding-top: 250px;
+        }
     }
 
     p {
@@ -49,8 +120,81 @@ const Content = styled.div`
         max-width: 380px;
     }
 
-    h1, p {
+    button {
+        background: rgba(0, 0, 0, 0.2);
+        border: 0px;
+        font-size: 16px;
+        padding: 12px 30px;
+        border-radius: 14px;
+        border: 1px solid rgba(255, 255, 255, 0.1);
+        color: white;
+        max-width: 280px;
+        backdrip-filter: blur(20px);
+
+        display: flex;
+        gap: 12px;
+        justify-content: center;
+        align-items: center;
+
+        transition: 1s;
+
+        :hover {
+            border: 1px solid rgba(255, 255, 255, 0.8);
+            transform: translateY(-3px);
+        }
+    }
+
+    h1,
+    p,
+    button {
         margin: 0 30px 0 100px;
     }
-`
+`;
 
+const Menu = styled.ul`
+    display: flex;
+    gap: 30px;
+    align-items: center;
+    margin: 0 30px 0 100px;
+    padding: 0;
+
+    li {
+        list-style: none;
+        margin: 0;
+
+        a {
+            text-decoration: none;
+            color: white;
+            padding: 8px 20px;
+            border: 1px solid rgba(255, 255, 255, 0);
+            transition: 1s;
+            border-radius: 14px;
+
+            :hover {
+                1px solid rgba(255, 255, 255, 0.2);
+            }
+        }
+    }
+`;
+
+const Social = styled.div`
+    position: absolute;
+    top: 150px;
+    left: 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    align-items: center;
+
+    div {
+        width: 1px;
+        height: 500px;
+        background: linear-gradient(
+            180deg,
+            #08b6f9 0%,
+            #6c56ef 33.57%,
+            #1306dd 65.86%,
+            #aa0eb2 100%
+        );
+    }
+`;
